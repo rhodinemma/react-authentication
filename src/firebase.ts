db893b6468a7202909302736515c955de694a017
rhodinemma <rhodinemma10@gmail.com>
2023-05-16 16:17:58 +0300

    feat: setup firebase

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBd6FDz9-aDS1UZX1mesxDqZzBJHga0dUA",
+  authDomain: "fir-auth-60de5.firebaseapp.com",
+  projectId: "fir-auth-60de5",
+  storageBucket: "fir-auth-60de5.appspot.com",
+  messagingSenderId: "388670450121",
+  appId: "1:388670450121:web:80c2fb42dd9f95ef9ca018"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
